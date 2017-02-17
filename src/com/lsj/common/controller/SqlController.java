729dfcb4666fc604c8701267502a62445789ff11
shakeel <lsj9383@163.com>
2017-02-18 00:06:21 +0800

    添加对应表javabean的生成

--- src/com/lsj/common/controller/SqlController.java
+++ src/com/lsj/common/controller/SqlController.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lsj.common.interceptor.Authority;
 import com.lsj.common.interceptor.AuthorityType;
@@ -19,22 +20,16 @@ public class SqlController {
 	private JdbcTemplate jt;
 	
 	@Authority(type=AuthorityType.NoValidata)
-	@RequestMapping("sqlview.do")
-	public String sqlview(){
-		return "common/sqlview";
-	}
-	
-	
-	@Authority(type=AuthorityType.NoValidata)
 	@RequestMapping("sql.do")
-	public String sql(Model model, String tableName){
+	public String sql(Model model, @RequestParam(defaultValue="")String tableName){
 		String sql = "SELECT COLUMN_NAME as cname, DATA_TYPE as dtype FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=? AND TABLE_NAME=?";
 		List<Map<String, Object>> mappers = jt.queryForList(sql, "cms", tableName);
 		String selectSql = getSelectSql(tableName, mappers);
 		String insertSql = getInsertSql(tableName, mappers);
+		String javabean = getJavabean(tableName, mappers);
 		model.addAttribute("selectSql", selectSql);
 		model.addAttribute("insertSql", insertSql);
-		model.addAttribute("javabean", null);
+		model.addAttribute("javabean", javabean);
 		return "common/sqlview";
 	}
 	
@@ -65,6 +60,28 @@ public class SqlController {
 		}
 		sb.deleteCharAt(sb.length()-1);
 		sb.append(")");
+		return sb.toString();
+	}
+	
+	private String getJavabean(String tableName, List<Map<String, Object>> mappers){
+		StringBuilder sb = new StringBuilder();
+		sb.append("public class "+sqlName2JavaName(tableName)+"{\n");
+		for(Map<String, Object> mapper : mappers){
+			String cname = mapper.get("cname").toString();
+			String dtype = mapper.get("dtype").toString();
+			
+			if(dtype.equals("varchar")){
+				sb.append("private String ");
+			}else if(dtype.equals("int")){
+				sb.append("private Integer ");
+			}else if(dtype.equals("datetime")){
+				sb.append("private Date ");
+			}else{
+				continue;
+			}
+			sb.append(sqlName2JavaName(cname)+";\n");
+		}
+		sb.append("}");
 		return sb.toString();
 	}
 	
